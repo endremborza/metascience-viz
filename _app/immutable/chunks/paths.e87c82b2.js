2cd6ec74f7365f7be7135d5474fbf82f8f0b6f66
Endre Márk Borza <endremborza@gmail.com>
2023-09-07 19:23:22 +0200

    Updates

--- _app/immutable/chunks/paths.e87c82b2.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_9klol3)==null?void 0:s.base)??"/metascience-viz";var e;const l=((e=globalThis.__sveltekit_9klol3)==null?void 0:e.assets)??a;export{l as a,a as b};
